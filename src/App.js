c2e846dab5561104cc1b1bed77f9662fd2194929
PhilCodesNow <philipmayojr@outlook.com>
2020-12-17 10:36:36 -0500

    basic app styling done, options setup from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './currencyRow';
+
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
+  }, [])
   return (
     <div className="App">
       <h1>Convert</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}/>
+      <div className="equals">=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}/>
     </div>
   );
 }
--- src/currencyRow.js
+++ src/currencyRow.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-export default function currencyRow() {
+
+export default function currencyRow(props) {
+    const {
+        currencyOptions
+    } = props
     return (
         <div>
-            
+            <input type="number" className="input"/>
+            <select>
+                {currencyOptions.map(option => (
+                    <option value={option} key={option}>{option}</option>
+                ))}
+            </select>
         </div>
     )
 }
